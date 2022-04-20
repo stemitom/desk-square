be53d6032d62890711b52c1f36f85eafa5e1eb40
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-04-20 20:44:25 +0100

    Created app accounts and events and added to INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Local Apps
+    'accounts',
+    'events',
 
     # Third Party Apps
     'rest_framework'
