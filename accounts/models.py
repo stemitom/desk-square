bda691ec6cb5eea58ac37c6663df6cbc56d007bf
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-04-26 13:38:52 +0100

    Tweak user model and made migrations

--- accounts/models.py
+++ accounts/models.py
@@ -5,14 +5,24 @@ from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
 
 # Create your models here.
-class CustomUser(AbstractUser):
+class User(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-    objects = CustomUserManager()
+    def get_full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.email
+
+    @property
+    def is_staff(self) -> bool:
+        return self.is_staff
+
+    def is_admin(self) -> bool:
+        return self.is_admin
+
+    objects = CustomUserManager()
--- config/settings.py
+++ config/settings.py
@@ -129,4 +129,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = 'accounts.User'
