c234e5ca28b9b8823c98a3841594b0948aefa3d8
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-04-26 15:01:52 +0100

    Added authentication backend

---
+++ accounts/backends.py
@@ -0,0 +1,16 @@
+import email
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+
+UserModel = get_user_model()
+
+class EmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = UserModel.objects.get(email=username)
+        except UserModel.DoesNotExist:
+            UserModel().set_password(password)
+            return
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
