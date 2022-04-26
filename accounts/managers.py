d729d7f70aa7d9dfd5e1f30e9a3faa5945ebbb81
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-04-26 13:06:44 +0100

    Created customusermanager and custom user model

---
+++ accounts/managers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_("The email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_superuser", True)
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff set to True"))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser set to True"))
+        return self.create_user(email, password, **extra_fields)
