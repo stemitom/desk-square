2f3c23e4d8ae9f8c26c22020e8420c4aa1125655
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-06-11 17:53:46 +0100

    Fixed environment issue for email host and port

--- config/settings.py
+++ config/settings.py
@@ -161,8 +161,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Email testing client
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = env("EMAIL_HOST")
-EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST = env("EMAIL_HOST") or "localhost"
+EMAIL_PORT = env("EMAIL_PORT") or 1025
 
 
 # Internationalization
